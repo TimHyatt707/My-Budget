ed4b42d1cfc23a57de58e0621ead88fa58959017
TimHyatt707 <timhyatt707@gmail.com>
2017-11-28 10:01:08 -0800

    removed log

--- src/redux/thunks/getAuthenticationProcess.js
+++ src/redux/thunks/getAuthenticationProcess.js
@@ -2,16 +2,17 @@ import getAuth from "./../../api/utils/getAuth";
 
 export default function getAuthenticationProcess() {
   return (dispatch, getState) => {
-    return getAuth().then(authentication => {
-      const token = authentication.token;
-      const userId = authentication.id;
-      if (!token) {
-        return null;
-      } else {
+    let token = localStorage.getItem("token");
+    if (!token) {
+      return null;
+    } else {
+      return getAuth().then(authentication => {
+        const token = authentication.token;
+        const userId = authentication.id;
         dispatch({ type: "SET_TOKEN", token });
         dispatch({ type: "SET_ID", userId });
         return authentication;
-      }
-    });
+      });
+    }
   };
 }
